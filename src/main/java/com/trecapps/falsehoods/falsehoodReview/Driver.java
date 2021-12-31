f8e7a97e47274a687dea3ef71131ea3b0c42b332
John Jacko <jljacko@outlook.com>
2021-12-30 19:03:06 -0700

    Added Entity Scan

--- src/main/java/com/trecapps/falsehoods/falsehoodReview/Driver.java
+++ src/main/java/com/trecapps/falsehoods/falsehoodReview/Driver.java
@@ -2,12 +2,14 @@ package com.trecapps.falsehoods.falsehoodReview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan({"com.trecapps.falsehoods.falsehoodReview","com.trecapps.falsehoods.falsehoodSearch"})
 @EnableJpaRepositories(basePackages = {"com.trecapps.base.FalsehoodModel.repos", "com.trecapps.base.InfoResource.repos"})
+@EntityScan(basePackages = {"com.trecapps.base.FalsehoodModel.models", "com.trecapps.base.InfoResource.models"})
 public class Driver {
 
     public static void main(String[] args) {
