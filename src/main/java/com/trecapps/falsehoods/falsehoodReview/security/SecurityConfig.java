dfeb3b36c6fa8d1fea549a1a0da17d669b2c00b0
John Jacko <jljacko@outlook.com>
2021-12-30 18:37:55 -0700

    Revamped AD beans

--- src/main/java/com/trecapps/falsehoods/falsehoodReview/security/SecurityConfig.java
+++ src/main/java/com/trecapps/falsehoods/falsehoodReview/security/SecurityConfig.java
@@ -15,17 +15,17 @@ import org.springframework.stereotype.Component;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    AADOAuth2UserService getAadoAuth2UserService(AADAuthenticationProperties aadAuthProps)
-    {
-        return new AADOAuth2UserService(aadAuthProps);
-    }
 
     @Autowired
+    SecurityConfig(AADAuthenticationProperties aadAuthProps,
+                    FalsehoodUserRepo falsehoodUserRepo)
+    {
+        aadoAuth2UserService = new AADOAuth2UserService(aadAuthProps);
+        this.falsehoodUserRepo = falsehoodUserRepo;
+    }
+    FalsehoodUserRepo falsehoodUserRepo;
     AADOAuth2UserService aadoAuth2UserService;
 
-    @Autowired
-    FalsehoodUserRepo falsehoodUserRepo;
 
     @Override
     protected void configure(HttpSecurity security) throws Exception
