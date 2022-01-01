ea3f09b4f513eec05fa613f258322b40ad651b78
John Jacko <jljacko@outlook.com>
2021-12-31 21:56:19 -0700

    Moved Repos from Dependencies to actual project

---
+++ src/main/java/com/trecapps/falsehoods/falsehoodReview/repos/PublicFalsehoodRecordsRepo.java
@@ -0,0 +1,41 @@
+package com.trecapps.falsehoods.falsehoodReview.repos;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trecapps.base.FalsehoodModel.models.PublicFalsehoodRecords;
+import com.trecapps.base.InfoResource.config.StorageClient;
+import com.trecapps.base.InfoResource.models.Record;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@Component
+public class PublicFalsehoodRecordsRepo// extends CosmosRepository<PublicFalsehoodRecords, BigInteger>
+{
+    @Autowired
+    StorageClient client;
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    public void save(PublicFalsehoodRecords records) throws JsonProcessingException {
+        if(records.getFalsehoodId() == null)
+            throw new NullPointerException("Null Falsehood Id Provided!");
+
+        String name = "Public-Falsehood-Records-" + records.getFalsehoodId();
+
+        client.SubmitJson(name, mapper.writeValueAsString(records.getRecords()), "Trec-Apps-Falsehood", "Falsehood");
+    }
+
+    public List<Record> retrieveRecords(BigInteger id) throws JsonProcessingException {
+        String name = "Public-Falsehood-Records-" + id;
+
+        String contents = client.getContents(name, "Falsehood").block();
+
+        return mapper.readValue(contents, new TypeReference<List<Record>>() {
+        });
+    }
+}
