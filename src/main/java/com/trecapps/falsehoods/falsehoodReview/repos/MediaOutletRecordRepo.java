5dc37315c06d64be121340b23653a149cb8b3296
John Jacko <jljacko@outlook.com>
2021-12-31 23:39:39 -0700

    Revert "Updated records repos to use the local Storage Client Class"
    This reverts commit 86a7522dae3ccc30cde0b1dd84c231b6a7724c4d.

--- src/main/java/com/trecapps/falsehoods/falsehoodReview/repos/MediaOutletRecordRepo.java
+++ src/main/java/com/trecapps/falsehoods/falsehoodReview/repos/MediaOutletRecordRepo.java
@@ -3,7 +3,7 @@ package com.trecapps.falsehoods.falsehoodReview.repos;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.trecapps.falsehoods.falsehoodReview.config.StorageClient;
+import com.trecapps.base.InfoResource.config.StorageClient;
 import com.trecapps.base.InfoResource.models.MediaOutletRecords;
 import com.trecapps.base.InfoResource.models.Record;
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/trecapps/falsehoods/falsehoodReview/repos/PublicFalsehoodRecordsRepo.java
+++ src/main/java/com/trecapps/falsehoods/falsehoodReview/repos/PublicFalsehoodRecordsRepo.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trecapps.base.FalsehoodModel.models.PublicFalsehoodRecords;
-import com.trecapps.falsehoods.falsehoodReview.config.StorageClient;
+import com.trecapps.base.InfoResource.config.StorageClient;
 import com.trecapps.base.InfoResource.models.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
